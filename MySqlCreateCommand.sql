fd2ee6dcf7edaf8f4ec97e39f3630aa31e2aed0e
Stephen White <37700241+Vukky97@users.noreply.github.com>
2019-06-13 00:48:59 +0200

    Bug Fixed

--- MySqlCreateCommand.sql
+++ MySqlCreateCommand.sql
@@ -2,11 +2,11 @@ CREATE TABLE CARS(
 	id INT NOT NULL auto_increment UNIQUE,
 	make VARCHAR(16) NOT NULL,
 	modell VARCHAR(20) NOT NULL,
-    engine VARCHAR(10) NOT NULL, 
+    	engine VARCHAR(10) NOT NULL, 
 	price int NOT NULL,
 	available boolean,
 	year INT NOT NULL
 	);
 
-	insert into cars (make,modell,year,engine,price,available)
+	insert into cars (make,modell,engine,price,available,year)
 	 values('BMW','E36','20i',850000,true,1998);
